a9d299dae3ff2bfdc4db2f766a18ac761103d271
AlexAnemogiannis <a.anemogiannis@sae.edu>
2020-01-30 15:20:14 +0200

    First Commit

---
+++ Assets/Editor/MeshPrimitiveWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+public class MeshPrimitiveWindow : Editor
+{
+    [MenuItem("GameObject/3D Object/Hex")]
+
+    private static void CreateHex() 
+    {
+        GameObject hex = new GameObject();
+        hex.AddComponent<MeshFilter>();
+        hex.AddComponent<MeshRenderer>();
+        HexMesh mesh = new HexMesh();
+        hex.GetComponent<MeshFilter>().mesh = mesh.HexMeshData();
+        hex.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Diffuse"));
+        hex.transform.position = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapGenerator : EditorWindow
+{
+    [MenuItem("Tools/HexMapGenerator")]
+
+    static void Init() 
+    {
+        MapGenerator window = (MapGenerator)EditorWindow.GetWindow(typeof(MapGenerator));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Base Settings", EditorStyles.boldLabel);
+    }
+}
